49b6a7ffd7b2e63f1b3d2011b80c9ea0fd48c2a6
GeonhyeongKim <geonhyeong.dev@gmail.com>
2023-03-27 17:39:32 +0900

    [Add] Pretendard Font

---
+++ Targets/MMM/Resources/MMMResource.swift
@@ -0,0 +1,13 @@
+//
+//  MMMResource.swift
+//  MMM
+//
+//  Created by geonhyeong on 2023/03/27.
+//  Copyright © 2023 Lab.M. All rights reserved.
+//
+
+import Foundation
+
+public typealias R = MMMResource
+
+public enum MMMResource { }

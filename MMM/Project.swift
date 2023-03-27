4c3836fcd7c09f7c405adc9caafb147822a51a9b
jwoo820 <pjwoo820@gmail.com>
2023-03-27 17:06:58 +0900

    [Add] init project

---
+++ MMM/Project.swift
@@ -0,0 +1,61 @@
+import ProjectDescription
+import ProjectDescriptionHelpers
+import MyPlugin
+
+/*
+                +-------------+
+                |             |
+                |     App     | Contains MMM App target and MMM unit-test target
+                |             |
+         +------+-------------+-------+
+         |         depends on         |
+         |                            |
+ +----v-----+                   +-----v-----+
+ |          |                   |           |
+ |   Kit    |                   |     UI    |   Two independent frameworks to share code and start modularising your app
+ |          |                   |           |
+ +----------+                   +-----------+
+
+ */
+
+// MARK: - Project
+
+// MARK: - Project
+private let appName = "MMM"
+private let bundleId = "com.lab.MMM"
+private let targetVersion = "14.0"
+private let organizationName = "Lab.M"
+
+// Local plugin loaded
+let localHelper = LocalHelper(name: "MyPlugin")
+
+let infoPlist: [String: InfoPlist.Value] = [
+    "CFBundleShortVersionString": "1.0",
+    "CFBundleVersion": "1",
+    "UIMainStoryboardFile": "",
+    "UILaunchStoryboardName": "LaunchScreen"
+    ]
+
+
+let project = Project(
+    name: appName,
+    organizationName: organizationName,
+    packages: [
+        .remote(url: "https://github.com/SnapKit/SnapKit.git", requirement: .upToNextMajor(from: "5.6.0")),
+        .remote(url: "https://github.com/devxoul/Then.git", requirement: .upToNextMajor(from: "3.0.0"))
+    ],
+    targets: [
+        Target(name: appName,
+               platform: .iOS,
+               product: .app,
+               bundleId: bundleId,
+               deploymentTarget: .iOS(targetVersion: targetVersion, devices: [.iphone]),
+               infoPlist: .extendingDefault(with: infoPlist),
+               sources: ["Targets/\(appName)/Sources/**"],
+               resources: ["Targets/\(appName)/Resources/**"],
+               dependencies: [
+                .package(product: "ReactorKit")
+               ]
+          )
+    ]
+)
---
+++ MMM/Targets/MMMKit/Tests/MMMKitTests.swift
@@ -0,0 +1,8 @@
+import Foundation
+import XCTest
+
+final class MMMKitTests: XCTestCase {
+    func test_example() {
+        XCTAssertEqual("MMMKit", "MMMKit")
+    }
+}

9dfafac8dc127f942ce1d07a1124d63e84fd557e
GeonhyeongKim <geonhyeong.dev@gmail.com>
2023-03-27 18:02:29 +0900

    [Add] Icon Assets

---
+++ Targets/MMM/Resources/Extensions/Ex+Icon.swift
@@ -0,0 +1,38 @@
+//
+//  Ex+Icon.swift
+//  MMM
+//
+//  Created by geonhyeong on 2023/03/27.
+//  Copyright © 2023 Lab.M. All rights reserved.
+//
+
+import UIKit
+
+public extension MMMResource {
+	enum Icon {
+		public static let arrowBack24 = UIImage(named: "iconArrowBack24", in: .module, compatibleWith: nil)
+		public static let arrowDown32 = UIImage(named: "iconArrowDown32", in: .module, compatibleWith: nil)
+		public static let arrowDropdown16 = UIImage(named: "iconArrowDropdown16", in: .module, compatibleWith: nil)
+		public static let arrowExpandLess16 = UIImage(named: "iconArrowExpandLess16", in: .module, compatibleWith: nil)
+		public static let arrowExpandMore16 = UIImage(named: "iconArrowExpandMore16", in: .module, compatibleWith: nil)
+		public static let arrowNext16 = UIImage(named: "iconArrowNext16", in: .module, compatibleWith: nil)
+		public static let arrowNext24 = UIImage(named: "iconArrowNext24", in: .module, compatibleWith: nil)
+		public static let arrowUp32 = UIImage(named: "iconArrowUp32", in: .module, compatibleWith: nil)
+		public static let camera48 = UIImage(named: "iconCamera48", in: .module, compatibleWith: nil)
+		public static let coinEarn40 = UIImage(named: "iconCoinEarn40", in: .module, compatibleWith: nil)
+		public static let coinPay40 = UIImage(named: "iconCoinPay40", in: .module, compatibleWith: nil)
+		public static let delete24 = UIImage(named: "iconDelete24", in: .module, compatibleWith: nil)
+		public static let highlightoff24 = UIImage(named: "iconHighlightoff24", in: .module, compatibleWith: nil)
+		public static let photoBlank64 = UIImage(named: "iconPhotoBlank64", in: .module, compatibleWith: nil)
+		public static let report16 = UIImage(named: "iconReport16", in: .module, compatibleWith: nil)
+		public static let star16 = UIImage(named: "iconStar16", in: .module, compatibleWith: nil)
+		public static let star24 = UIImage(named: "iconStar24", in: .module, compatibleWith: nil)
+		public static let star36 = UIImage(named: "iconStar36", in: .module, compatibleWith: nil)
+		public static let star48 = UIImage(named: "iconStar48", in: .module, compatibleWith: nil)
+		
+		// MARK: - FAB
+		public static let onboarding1 = UIImage(named: "iconOnboarding1", in: .module, compatibleWith: nil)
+		public static let onboarding2 = UIImage(named: "iconOnboarding2", in: .module, compatibleWith: nil)
+		public static let mypageBg = UIImage(named: "iconMypageBg", in: .module, compatibleWith: nil)
+	}
+}

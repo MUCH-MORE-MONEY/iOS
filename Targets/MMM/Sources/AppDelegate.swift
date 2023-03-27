92a42b792b218de31933d4f32b7dc2b1ee5a6dff
GeonhyeongKim <geonhyeong.dev@gmail.com>
2023-03-27 17:19:11 +0900

    [Del] Reactorkit

--- Targets/MMM/Sources/AppDelegate.swift
+++ Targets/MMM/Sources/AppDelegate.swift
@@ -5,10 +5,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
-    func application(
-        _ application: UIApplication,
-        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil
-    ) -> Bool {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
         window = UIWindow(frame: UIScreen.main.bounds)
         let viewController = UIViewController()
         viewController.view.backgroundColor = .green
@@ -18,5 +15,4 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
         return true
     }
-
 }

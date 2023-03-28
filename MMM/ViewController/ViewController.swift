17b51769db96628f54ed27ac71de69161b985e0d
GeonhyeongKim <geonhyeong.dev@gmail.com>
2023-03-28 11:08:53 +0900

    [Refactor] 프로젝트 구조 변경

---
+++ MMM/ViewController/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MMM
+//
+//  Created by geonhyeong on 2023/03/28.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
--- Targets/MMMKit/Tests/MMMKitTests.swift
+++
@@ -1,8 +0,0 @@
-import Foundation
-import XCTest
-
-final class MMMKitTests: XCTestCase {
-    func test_example() {
-        XCTAssertEqual("MMMKit", "MMMKit")
-    }
-}

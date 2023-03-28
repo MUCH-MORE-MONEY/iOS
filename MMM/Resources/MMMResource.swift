559c71124b6399c8ed14c0eb09509a5495edacc2
GeonhyeongKim <geonhyeong.dev@gmail.com>
2023-03-28 17:10:25 +0900

    [Feat] Resource 접근을 위한 대리자(enum) 선언

---
+++ MMM/Resources/MMMResource.swift
@@ -0,0 +1,12 @@
+//
+//  MMMResource.swift
+//  MMM
+//
+//  Created by geonhyeong on 2023/03/28.
+//
+
+import Foundation
+
+public typealias R = MMMResource
+
+public enum MMMResource { }
--- MMM/Sources/ViewController/ViewController.swift
+++ MMM/Sources/ViewController/ViewController.swift
@@ -8,28 +8,28 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+	
 	let label = UILabel()
-
+	
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		setup()
 		style()
 		layout()
 	}
-  }
+}
 
-  //MARK: - Style & Layouts
-  extension ViewController {
-
+//MARK: - Style & Layouts
+extension ViewController {
+	
 	private func setup() {
 		// 초기 셋업할 코드들
 	}
-
+	
 	private func style() {
 		// [view]
 		view.backgroundColor = .systemBackground
-
+		
 		// [Label]
 		label.translatesAutoresizingMaskIntoConstraints = false
 		label.numberOfLines = 0
@@ -38,19 +38,15 @@ class ViewController: UIViewController {
 		label.text = "Welcome to \n ViewController"
 		
 		view.addSubview(label)
-
-
 	}
-
+	
 	private func layout() {
-
+		
 		// [label] 기본 중앙 배치
 		NSLayoutConstraint.activate([
 			label.centerXAnchor.constraint(equalTo: view.centerXAnchor),
 			label.centerYAnchor.constraint(equalTo: view.centerYAnchor)
 		])
 	}
-
-
 }
 

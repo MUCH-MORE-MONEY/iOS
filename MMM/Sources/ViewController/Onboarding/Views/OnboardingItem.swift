2f4f54bafc26edf49c5668193b0c3fad8e4e740e
jwoo820 <pjwoo820@gmail.com>
2023-03-31 15:16:25 +0900

    [Add] onboardingitem 구조체 추가

---
+++ MMM/Sources/ViewController/Onboarding/Views/OnboardingItem.swift
@@ -0,0 +1,15 @@
+//
+//  OnboardingItem.swift
+//  MMM
+//
+//  Created by Park Jungwoo on 2023/03/31.
+//
+
+import UIKit
+
+struct OnboardingItem {
+    let image: UIImageView
+    let mainLabel1: String
+    let mainLabel2: String
+    let subLabel: String
+}

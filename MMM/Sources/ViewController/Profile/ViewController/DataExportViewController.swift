845134b8b2c8aa39dddf999ff3b0e7ada05b4c69
jwoo820 <pjwoo820@gmail.com>
2023-04-04 09:38:30 +0900

    [Add] 데이터 내보내기 뷰 추가

---
+++ MMM/Sources/ViewController/Profile/ViewController/DataExportViewController.swift
@@ -0,0 +1,29 @@
+//
+//  DataExportViewController.swift
+//  MMM
+//
+//  Created by Park Jungwoo on 2023/04/04.
+//
+
+import UIKit
+
+class DataExportViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

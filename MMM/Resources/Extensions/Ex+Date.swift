faf1ad131ef5d4a291e5185e6fa6415be8cb5aea
GeonhyeongKim <geonhyeong.dev@gmail.com>
2023-04-19 00:58:13 +0900

    [Feat] Date format에 따라 string으로 바꿔주는 extensions 추가

---
+++ MMM/Resources/Extensions/Ex+Date.swift
@@ -0,0 +1,27 @@
+//
+//  Ex+Date.swift
+//  MMM
+//
+//  Created by geonhyeong on 2023/04/18.
+//
+
+import Foundation
+
+extension Date {
+	func getFormattedDate(format: String) -> String {
+		let dateformat = DateFormatter()
+		dateformat.locale = Locale(identifier: "ko_KR")
+		dateformat.timeZone = TimeZone(abbreviation: "KST")
+		dateformat.dateFormat = format
+		return dateformat.string(from: self)
+	}
+	
+	/// format: yyyy-MM-dd
+	func getFormattedDefault() -> String {
+		let dateformat = DateFormatter()
+		dateformat.locale = Locale(identifier: "ko_KR")
+		dateformat.timeZone = TimeZone(abbreviation: "KST")
+		dateformat.dateFormat = "yyyy-MM-dd"
+		return dateformat.string(from: self)
+	}
+}

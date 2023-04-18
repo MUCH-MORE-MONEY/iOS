4427d3de98d6614601b155752abbd3246b2e084d
jwoo820 <pjwoo820@gmail.com>
2023-04-18 15:22:08 +0900

    [Feat] TabBar 그림자 효과 추가

--- MMM/Resources/Extensions/Ex+UIControl.swift
+++ MMM/Resources/Extensions/Ex+UIControl.swift
@@ -53,4 +53,24 @@ extension UIControl {
 			_ = subscriber?.receive(control)
 		}
 	}
+    
+    
+    func addAction(for controlEvents: UIControl.Event = .touchUpInside, _ closure: @escaping () -> ()) {
+        @objc class ClosureSleeve: NSObject {
+            let closure: () -> ()
+            
+            init(_ closure: @escaping () -> ()) {
+                self.closure = closure
+            }
+            
+            @objc func invoke() {
+                closure()
+            }
+        }
+        
+        let sleeve = ClosureSleeve(closure)
+        addTarget(sleeve, action: #selector(ClosureSleeve.invoke), for: controlEvents)
+        objc_setAssociatedObject(self, "\(UUID())", sleeve, objc_AssociationPolicy.OBJC_ASSOCIATION_RETAIN)
+    }
+
 }

be0696bef49ed7425558474af22695492add1beb
jwoo820 <pjwoo820@gmail.com>
2023-05-01 15:19:54 +0900

    [Chore] 필드 타입 리펙토링

--- MMM/Sources/Services/APIClient.swift
+++ MMM/Sources/Services/APIClient.swift
@@ -9,6 +9,22 @@ import Foundation
 import Combine
 
 
+
+enum HTTPHeaderField: String {
+    case authentication = "Authentication"
+    case contentType = "Content-Type"
+    case acceptType = "Accept"
+    case acceptEncoding = "Accept-Encoding"
+    case authorization = "Authorization"
+    case acceptLanguage = "Accept-Language"
+    case userAgent = "User-Agent"
+}
+
+enum ContentType: String {
+    case json = "application/json"
+    case xwwwformurlencoded = "application/x-www-form-urlencoded"
+}
+
 /// HTTP 메서드
 enum HTTPMethod: String {
     case get     = "GET"
--- MMM/Sources/Services/APIConstants.swift
+++ MMM/Sources/Services/APIConstants.swift
@@ -11,18 +11,3 @@ enum APIConstants {
     static let baseURL = "http://ec2-13-209-4-42.ap-northeast-2.compute.amazonaws.com:8080"
 }
 
-enum HTTPHeaderField: String {
-    case authentication = "Authentication"
-    case contentType = "Content-Type"
-    case acceptType = "Accept"
-    case acceptEncoding = "Accept-Encoding"
-    case authorization = "Authorization"
-    case acceptLanguage = "Accept-Language"
-    case userAgent = "User-Agent"
-}
-
-enum ContentType: String {
-    case json = "application/json"
-    case xwwwformurlencoded = "application/x-www-form-urlencoded"
-}
-

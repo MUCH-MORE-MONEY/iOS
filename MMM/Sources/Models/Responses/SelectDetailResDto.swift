7defa0aedce19e0f44fe3e16ee942a685764e1ea
jwoo820 <pjwoo820@gmail.com>
2023-05-11 11:08:23 +0900

    [Feat] selectedDetail response

---
+++ MMM/Sources/Models/Responses/SelectDetailResDto.swift
@@ -0,0 +1,12 @@
+//
+//  SelectDetailReqResDto.swift
+//  MMM
+//
+//  Created by Park Jungwoo on 2023/05/11.
+//
+
+import Foundation
+
+struct SelectDetailResDto {
+    var selectDetailOutputDto: EconomicActivityDetail
+}

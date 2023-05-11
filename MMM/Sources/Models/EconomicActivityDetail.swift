1165b1f7e5def24d8fc4280193c38f25b7023973
jwoo820 <pjwoo820@gmail.com>
2023-05-11 11:07:25 +0900

    [Feat] 경제활동디테일 모델

---
+++ MMM/Sources/Models/EconomicActivityDetail.swift
@@ -0,0 +1,34 @@
+//
+//  EconomicActivityDetail.swift
+//  MMM
+//
+//  Created by Park Jungwoo on 2023/05/11.
+//
+
+import Foundation
+
+struct EconomicActivityDetail {
+    let id: String
+    let amount: String
+    let type: String
+    let groupName: String
+    let groupNo: String
+    let title: String
+    let memo: String
+    let createAt: String
+    let imageUrl: String
+    let star: Int
+    
+    enum CodingKeys: String, CodingKey {
+        case id = "economicActivityNo"
+        case amount = "economicActivityAmt"
+        case type = "economicActivityDvcd"
+        case groupName = "economicActivityGroupNm"
+        case groupNo = "economicActivityGroupNo"
+        case title = "economicActivityMm"
+        case memo = "economicActivityNm"
+        case createAt = "economicActivityYMD"
+        case imageUrl = "fileUrl"
+        case star = "valueScore"
+    }
+}

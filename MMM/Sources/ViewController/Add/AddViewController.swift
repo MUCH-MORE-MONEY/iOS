4ca1c63f37c4b9bbdd04302568009c64aadc4937
GeonhyeongKim <geonhyeong.dev@gmail.com>
2023-05-29 15:03:18 +0900

    [Feat] Label과 그 오른쪽에 check imageView를 넣기

--- MMM/Sources/ViewController/Add/AddViewController.swift
+++ MMM/Sources/ViewController/Add/AddViewController.swift
@@ -15,7 +15,10 @@ final class AddViewController: BaseViewController {
 	private lazy var cancellable: Set<AnyCancellable> = .init()
 
 	// MARK: - UI Components
-	
+	private lazy var typeLabel = UILabel()
+	private lazy var dateLabel = UILabel()
+	private lazy var priceLabel = UILabel()
+
 	public init() {
 		super.init(nibName: nil, bundle: nil)
 	}
@@ -54,8 +57,47 @@ private extension AddViewController {
 		view.backgroundColor = R.Color.gray900
 		title = "경제활동 추가"
 
+		typeLabel = typeLabel.then {
+			$0.text = "이 경제활동의 성격은"
+			$0.font = R.Font.title3
+			$0.textColor = R.Color.gray200
+			$0.textAlignment = .left
+			$0.addImageViewOnRight(image: R.Icon.checkOrange24)
+		}
+		
+		dateLabel = dateLabel.then {
+			$0.text = "이 경제활동을 한 날짜는"
+			$0.font = R.Font.title3
+			$0.textColor = R.Color.gray200
+			$0.textAlignment = .left
+			$0.addImageViewOnRight(image: R.Icon.checkOrange24)
+		}
+		
+		priceLabel = priceLabel.then {
+			$0.text = "이 경제활동에 사용된 금액은"
+			$0.font = R.Font.title3
+			$0.textColor = R.Color.gray200
+			$0.textAlignment = .left
+			$0.addImageViewOnRight(image: R.Icon.checkOrange24)
+		}
 	}
 	
 	private func setLayout() {
+		view.addSubviews(typeLabel, dateLabel, priceLabel)
+		
+		typeLabel.snp.makeConstraints {
+			$0.top.equalTo(view.safeAreaLayoutGuide).inset(40)
+			$0.leading.trailing.lessThanOrEqualTo(view.safeAreaLayoutGuide).inset(24)
+		}
+		
+		dateLabel.snp.makeConstraints {
+			$0.top.equalTo(view.safeAreaLayoutGuide).inset(155)
+			$0.leading.trailing.lessThanOrEqualTo(view.safeAreaLayoutGuide).inset(24)
+		}
+		
+		priceLabel.snp.makeConstraints {
+			$0.top.equalTo(view.safeAreaLayoutGuide).inset(277)
+			$0.leading.trailing.lessThanOrEqualTo(view.safeAreaLayoutGuide).inset(24)
+		}
 	}
 }

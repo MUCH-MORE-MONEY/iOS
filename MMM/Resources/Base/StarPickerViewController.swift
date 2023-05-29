512663db2db143ddd43f7a7819847ed5de11958f
jwoo820 <pjwoo820@gmail.com>
2023-05-29 19:08:34 +0900

    [Feat] starPickerView 제스처 기능

--- MMM/Resources/Base/StarPickerViewController.swift
+++ MMM/Resources/Base/StarPickerViewController.swift
@@ -27,10 +27,13 @@ class StarPickerViewController: UIViewController {
     // MARK: - Properties
     private var cancellable = Set<AnyCancellable>()
     weak var delegate: BottomSheetChild?
+    
+    private let feedbackGenerator = UISelectionFeedbackGenerator()
+    private var selectedRate: Int = 3
     override func viewDidLoad() {
         super.viewDidLoad()
         setup()
-
+        
     }
 }
 
@@ -39,6 +42,7 @@ extension StarPickerViewController {
     private func setup() {
         setAttribute()
         setLayout()
+//        bind()
     }
     
     private func setAttribute() {
@@ -47,10 +51,13 @@ extension StarPickerViewController {
         
         headerStackView.addArrangedSubviews(titleLabel, checkButton)
         
-        starList.forEach {
-            $0.contentMode = .scaleAspectFit
-            starStackView.addArrangedSubview($0)
-        }
+        createStars()
+        //        starList.enumerated().forEach { (index, value) in
+        //            value.contentMode = .scaleAspectFit
+        //            value.isUserInteractionEnabled = true
+        //            value.tag = index
+        //            starStackView.addArrangedSubview(value)
+        //        }
         
         headerStackView = headerStackView.then {
             $0.axis = .horizontal
@@ -85,6 +92,8 @@ extension StarPickerViewController {
             $0.axis = .horizontal
             $0.spacing = 23
             $0.distribution = .fillEqually
+            let tapGesture = UITapGestureRecognizer(target: self, action: #selector(selectStarRate))
+            $0.addGestureRecognizer(tapGesture)
         }
     }
     
@@ -109,6 +118,99 @@ extension StarPickerViewController {
     
     // MARK: - Bind
     private func bind() {
+        starStackView.gesturePublisher()
+            .receive(on: DispatchQueue.main)
+            .sink { [weak self] gesture in
+                guard let self = self else { return }
+                //                let gesture1 = UITapGestureRecognizer()
+                //                selectStarRate(gesture: gesture1)
+                //                print(gesture)
+                
+                switch gesture {
+                case let .tap(tapRecognizer):
+                    print("Tapped !")
+                    let location = tapRecognizer.location(in: tapRecognizer.view)
+                    print(location)
+                case let .swipe(swipeRecognizer):
+                    print("Swiped !")
+                    let location = swipeRecognizer.location(in: swipeRecognizer.view)
+                    print(location)
+                case let .longPress(longPressRecognizer):
+                    print("Long pressed !")
+                case let .pan(panRecognizer):
+                    print("Panned !")
+                case let .pinch(pinchRecognizer):
+                    print("Pinched !")
+                case let .edge(edgesRecognizer):
+                    print("Panned edges !")
+                }
+                
+                
+                
+                let location = gesture.get().location(in: gesture.get().view)
+                print("location", location)
+//                selectStarRate(gesture: gesture.get() as! UITapGestureRecognizer)
+            }.store(in: &cancellable)
         
+        //        starList.forEach {
+        //            $0.gesturePublisher()
+        //                .receive(on: DispatchQueue.main)
+        //                .sink { gesture in
+        ////                    self.
+        //                }
+        //        }
+    }
+}
+
+// MARK: - Action
+extension StarPickerViewController {
+    @objc func selectStarRate(gesture: UITapGestureRecognizer) {
+        
+        
+        
+        let location = gesture.location(in: starStackView)
+        print(location)
+        //        let location = gesture.location(in: gesture.view)
+        //        print(gesture.location(in: gesture.view))
+        
+        //        print(location)
+        let starWidth = starStackView.bounds.width / CGFloat(starList.count)
+        let rate = Int(location.x / starWidth) + 1
+        /// if current star doesn't match selectedRate then we change our rating
+        if rate != self.selectedRate {
+            feedbackGenerator.selectionChanged()
+            self.selectedRate = rate
+        }
+        
+        /// loop through starsContainer arrangedSubviews and
+        /// look for all Subviews of type UIImageView and change
+        /// their isHighlighted state (icons depend on it)
+        starStackView.arrangedSubviews.forEach {
+            guard let starImageView = $0 as? UIImageView else {
+                return
+            }
+            
+            starImageView.isHighlighted = starImageView.tag <= rate
+        }
+    }
+}
+
+// MARK: - UI Function
+extension StarPickerViewController {
+    private func createStars() {
+        /// loop through the number of our stars and add them to the stackView (starsContainer)
+        for index in 1...5 {
+            let star = makeStarIcon()
+            star.tag = index
+            starStackView.addArrangedSubview(star)
+        }
+    }
+    
+    private func makeStarIcon() -> UIImageView {
+        /// declare default icon and highlightedImage
+        let imageView = UIImageView(image: R.Icon.iconStarGray48, highlightedImage: R.Icon.iconStarBlack48)
+        imageView.contentMode = .scaleAspectFit
+        imageView.isUserInteractionEnabled = true
+        return imageView
     }
 }

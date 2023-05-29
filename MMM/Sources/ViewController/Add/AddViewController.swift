da0800c41a93f817918d7cf517f504ac8417db89
GeonhyeongKim <geonhyeong.dev@gmail.com>
2023-05-29 14:26:50 +0900

    [Feat] 경제활동 추가 첫 VC

--- MMM/Sources/ViewController/Add/AddViewController.swift
+++ MMM/Sources/ViewController/Add/AddViewController.swift
@@ -6,12 +6,56 @@
 //
 
 import UIKit
+import Combine
+import Then
+import SnapKit
 
-class AddViewController: UIViewController {
+final class AddViewController: BaseViewController {
+	// MARK: - Properties
+	private lazy var cancellable: Set<AnyCancellable> = .init()
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        view.backgroundColor = .blue
-        // Do any additional setup after loading the view.
-    }
+	// MARK: - UI Components
+	
+	public init() {
+		super.init(nibName: nil, bundle: nil)
+	}
+	
+	// Compile time에 error를 발생시키는 코드
+	@available(*, unavailable)
+	required init?(coder: NSCoder) {
+		fatalError("init(coder:) has not been implemented")
+	}
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		setup()		// 초기 셋업할 코드들
+	}
+}
+//MARK: - Action
+extension AddViewController {
+	
+}
+//MARK: - Style & Layouts
+private extension AddViewController {
+	// 초기 셋업할 코드들
+	private func setup() {
+		bind()
+		setAttribute()
+		setLayout()
+	}
+	
+	private func bind() {
+		//MARK: input
+		//MARK: output
+	}
+	
+	private func setAttribute() {
+		// [view]
+		view.backgroundColor = R.Color.gray900
+		title = "경제활동 추가"
+
+	}
+	
+	private func setLayout() {
+	}
 }

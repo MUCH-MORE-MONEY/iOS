3900a828b5d33d1c400a75a2e816df113ff87064
GeonhyeongKim <geonhyeong.dev@gmail.com>
2023-06-06 08:46:50 +0900

    [Feat] 위젯 기본 target

---
+++ MMMWidget/MMMWidgetBundle.swift
@@ -0,0 +1,17 @@
+//
+//  MMMWidgetBundle.swift
+//  MMMWidget
+//
+//  Created by geonhyeong on 2023/06/06.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct MMMWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        MMMWidget()
+        MMMWidgetLiveActivity()
+    }
+}

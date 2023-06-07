a8a97402f3635ecff62ba4a922c3dedb0bfe5ae5
jwoo820 <pjwoo820@gmail.com>
2023-06-07 14:19:08 +0900

    [Add] 위젯 폰트 파일 추가

---
+++ MMMWidget/Resources/Extensions/Ex+Font.swift
@@ -0,0 +1,81 @@
+//
+//  Ex+Font.swift
+//  MMM
+//
+//  Created by geonhyeong on 2023/06/06.
+//
+
+import SwiftUI
+
+public extension MMMResource {
+	enum Fonts {
+		public enum Family: String, CaseIterable {
+			case regular = "Regular"
+			case black = "Black"
+			case medium = "Medium"
+			case thin = "Thin"
+			case light = "Light"
+			case extraLight = "ExtraLight"
+			case bold = "Bold"
+			case semibold = "SemiBold"
+			case extraBold = "ExtraBold"
+		}
+		
+		public static func prtendard(family: Family = .regular, size: CGFloat = 11) -> Font {
+			return .custom("Pretendard-\(family.rawValue)", size: size)
+		}
+		
+		// TODO: - lineHeight
+		// MARK: - h0
+		/// Weight : semibold, Size : 36
+		public static let h0 = prtendard(family: .semibold, size: 36)
+		// MARK: - h1
+		/// Weight : bold, Size : 28
+		public static let h1 = prtendard(family: .bold, size: 28)
+		// MARK: - h2
+		/// Weight : bold, Size : 24
+		public static let h2 = prtendard(family: .bold, size: 24)
+		// MARK: - h3
+		/// Weight : regular, Size : 28
+		public static let h3 = prtendard(family: .regular, size: 28)
+		// MARK: - h4
+		/// Weight : semibold, Size : 20
+		public static let h4 = prtendard(family: .semibold, size: 20)
+		// MARK: - h5
+		/// Weight : bold, Size : 20
+		public static let h5 = prtendard(family: .bold, size: 20)
+		// MARK: - title1
+		/// Weight : bold, Size : 18
+		public static let title1 = prtendard(family: .bold, size: 18)
+		// MARK: - title2
+		/// Weight : semibold, Size : 18
+		public static let title2 = prtendard(family: .semibold, size: 18)
+		// MARK: - title3
+		/// Weight : bold, Size : 16
+		public static let title3 = prtendard(family: .bold, size: 16)
+		// MARK: - body3
+		/// Weight: regular, Size : 18
+		public static let body0 = prtendard(family: .regular, size: 18)
+		// MARK: - body1
+		/// Weight : regular, Size : 16
+		public static let body1 = prtendard(family: .regular, size: 16)
+		// MARK: - body2
+		/// Weight : bold, Size : 14
+		public static let body2 = prtendard(family: .bold, size: 14)
+		// MARK: - body3
+		/// Weight : regular, Size : 14
+		public static let body3 = prtendard(family: .regular, size: 14)
+		// MARK: - body4
+		/// Weight : bold, Size : 12
+		public static let body4 = prtendard(family: .bold, size: 12)
+		// MARK: - body5
+		/// Weight : regular, Size : 12
+		public static let body5 = prtendard(family: .regular, size: 12)
+		// MARK: - caption1
+		/// Weight : bold, Size : 11
+		public static let caption1 = prtendard(family: .bold, size: 11)
+		// MARK: - caption2
+		/// Weight : regular, Size : 11
+		public static let caption2 = prtendard(family: .regular, size: 11)
+	}
+}

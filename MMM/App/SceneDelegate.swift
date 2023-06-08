6be1d5921e7dea33a45d6c4a7ff500014d89ea6c
GeonhyeongKim <geonhyeong.dev@gmail.com>
2023-06-08 14:25:17 +0900

    [Hotfix] 코드 정렬

--- MMM/App/SceneDelegate.swift
+++ MMM/App/SceneDelegate.swift
@@ -8,7 +8,7 @@
 import UIKit
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
-
+	
 	var window: UIWindow?
 	var onboarding: OnboardingViewController!
 	
@@ -22,26 +22,26 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 		
 		onboarding = OnboardingViewController()
 		// 로그인이 되어 있을 경우
-        if Constants.getKeychainValue(forKey: Constants.KeychainKey.token) != nil {
-            let mainViewController = TabBarController(widgetIndex: 0)
-			      if let url = connectionOptions.urlContexts.first?.url {
-				    
-            window?.rootViewController = mainViewController
-          } else {
-            window?.rootViewController = mainViewController
-          }
-        } else {
-            let mainViewController = onboarding
-            window?.rootViewController = mainViewController
-        }
-        
-        // add 위젯으로 들어왔을 경우
-//        let mainViewController = TabBarController(widgetIndex: 1)
-        
+		if Constants.getKeychainValue(forKey: Constants.KeychainKey.token) != nil {
+			let mainViewController = TabBarController(widgetIndex: 0)
+			if let url = connectionOptions.urlContexts.first?.url {
+				
+				window?.rootViewController = mainViewController
+			} else {
+				window?.rootViewController = mainViewController
+			}
+		} else {
+			let mainViewController = onboarding
+			window?.rootViewController = mainViewController
+		}
+		
+		// add 위젯으로 들어왔을 경우
+		//        let mainViewController = TabBarController(widgetIndex: 1)
+		
 		// ViewController 초기화
 		
 		// MARK: Window 구성
-
+		
 		// 화면에 띄울 Root 뷰 컨트롤러 지정
 		
 		window?.backgroundColor = .systemBackground
@@ -61,35 +61,35 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 			window?.rootViewController = mainViewController
 		}
 	}
-
+	
 	func sceneDidDisconnect(_ scene: UIScene) {
 		// Called as the scene is being released by the system.
 		// This occurs shortly after the scene enters the background, or when its session is discarded.
 		// Release any resources associated with this scene that can be re-created the next time the scene connects.
 		// The scene may re-connect later, as its session was not necessarily discarded (see `application:didDiscardSceneSessions` instead).
 	}
-
+	
 	func sceneDidBecomeActive(_ scene: UIScene) {
 		// Called when the scene has moved from an inactive state to an active state.
 		// Use this method to restart any tasks that were paused (or not yet started) when the scene was inactive.
 	}
-
+	
 	func sceneWillResignActive(_ scene: UIScene) {
 		// Called when the scene will move from an active state to an inactive state.
 		// This may occur due to temporary interruptions (ex. an incoming phone call).
 	}
-
+	
 	func sceneWillEnterForeground(_ scene: UIScene) {
 		// Called as the scene transitions from the background to the foreground.
 		// Use this method to undo the changes made on entering the background.
 	}
-
+	
 	func sceneDidEnterBackground(_ scene: UIScene) {
 		// Called as the scene transitions from the foreground to the background.
 		// Use this method to save data, release shared resources, and store enough scene-specific state information
 		// to restore the scene back to its current state.
 	}
-
-
+	
+	
 }
 

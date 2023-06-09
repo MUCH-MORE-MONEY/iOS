438e851fc0a984a24f19f8325e583fea25f426d9
GeonhyeongKim <geonhyeong.dev@gmail.com>
2023-06-09 16:21:40 +0900

    [Feat] Bottom Sheet 높이 변경

--- MMM/Sources/ViewController/HomeDetail/EditActivityViewController.swift
+++ MMM/Sources/ViewController/HomeDetail/EditActivityViewController.swift
@@ -278,7 +278,7 @@ extension EditActivityViewController {
         let bottomSheetVC = BottomSheetViewController(contentViewController: picker)
         picker.delegate = bottomSheetVC
         bottomSheetVC.modalPresentationStyle = .overFullScreen
-        bottomSheetVC.setSetting(height: 210, isDrag: false)
+        bottomSheetVC.setSetting(height: 230, isDrag: false)
         self.present(bottomSheetVC, animated: false, completion: nil) // fasle(애니메이션 효과로 인해 부자연스럽움 제거)
     }
     

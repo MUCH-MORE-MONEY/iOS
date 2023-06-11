b876dd860bd8fc8fe2eda75cfdb1daa29bb3ffc5
jwoo820 <pjwoo820@gmail.com>
2023-06-11 13:36:15 +0900

    [Add] star 이미지 변경

--- MMM/Resources/Base/BaseAddActivityViewController.swift
+++ MMM/Resources/Base/BaseAddActivityViewController.swift
@@ -71,7 +71,8 @@ extension BaseAddActivityViewController {
             $0.font = R.Font.body0
             $0.textColor = R.Color.gray200
             $0.attributedPlaceholder = NSAttributedString(string: "경제활동의 제목을 입력해주세요.", attributes: [NSAttributedString.Key.foregroundColor : R.Color.gray400])
-            
+            $0.keyboardType = .webSearch
+            $0.delegate = self
         }
         
         starStackView = starStackView.then {
@@ -100,6 +101,7 @@ extension BaseAddActivityViewController {
             $0.sizeToFit()
             $0.isScrollEnabled = false
             $0.backgroundColor = R.Color.gray100
+            $0.keyboardType = .webSearch
         }
         
         saveButton = saveButton.then {
@@ -152,7 +154,8 @@ extension BaseAddActivityViewController {
         mainImageView.snp.makeConstraints {
             $0.top.equalTo(starStackView.snp.bottom).offset(16)
             $0.width.equalTo(view.safeAreaLayoutGuide).offset(-48)
-            $0.height.equalTo(mainImageView.image!.size.height * view.frame.width / mainImageView.image!.size.width)
+            $0.height.equalTo(mainImageView.snp.width)
+//            $0.height.equalTo(mainImageView.image!.size.height * view.frame.width / mainImageView.image!.size.width)
             $0.left.right.equalToSuperview()
         }
         
@@ -197,4 +200,12 @@ extension BaseAddActivityViewController {
             $0.bottom.equalToSuperview()
         }
     }
+}
+
+// MARK: - Textfield Delegate
+extension BaseAddActivityViewController: UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+            textField.resignFirstResponder() // 키보드 내리기
+            return true
+        }
 }

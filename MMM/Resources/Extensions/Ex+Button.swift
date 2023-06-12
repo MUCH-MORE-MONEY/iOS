a98bef7c67d2562b7e7290dc20b214c83f01d56c
jwoo820 <pjwoo820@gmail.com>
2023-06-12 22:31:16 +0900

    [Feat] 버튼 라벨은 이미지 아래로 변경해주는 기능

--- MMM/Resources/Extensions/Ex+Button.swift
+++ MMM/Resources/Extensions/Ex+Button.swift
@@ -29,4 +29,26 @@ extension UIButton {
         self.layer.shadowOffset = CGSize(width: 0, height: 2)
         self.layer.shadowRadius = 8
     }
+    
+    
+    func alignTextBelow(spacing: CGFloat = 4.0) {
+            guard let image = self.imageView?.image else {
+                return
+            }
+
+            guard let titleLabel = self.titleLabel else {
+                return
+            }
+
+            guard let titleText = titleLabel.text else {
+                return
+            }
+
+            let titleSize = titleText.size(withAttributes: [
+                NSAttributedString.Key.font: titleLabel.font as Any
+            ])
+
+            titleEdgeInsets = UIEdgeInsets(top: spacing, left: -image.size.width, bottom: -image.size.height, right: 0)
+            imageEdgeInsets = UIEdgeInsets(top: -(titleSize.height + spacing), left: 0, bottom: 0, right: -titleSize.width)
+        }
 }

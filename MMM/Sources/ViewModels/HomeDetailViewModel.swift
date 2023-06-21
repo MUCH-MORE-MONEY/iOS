c4260bbec75c5f353167c7212b9eabe710ec3066
GeonhyeongKim <geonhyeong.dev@gmail.com>
2023-06-21 19:43:47 +0900

    [Refactor] HomeDetailViewModel

--- MMM/Sources/ViewModels/HomeDetailViewModel.swift
+++ MMM/Sources/ViewModels/HomeDetailViewModel.swift
@@ -10,57 +10,57 @@ import Combine
 import WidgetKit
 
 final class HomeDetailViewModel {
-    // MARK: - Property Wrapper
-    @Published var detailActivity: SelectDetailResDto?
-    @Published var hasImage = false
-    @Published var mainImage: UIImage?
-    @Published var isShowToastMessage = false
-    @Published var isLoading = true
-    // MARK: - Porperties
-    private var cancellable: Set<AnyCancellable> = []
+	// MARK: - Property Wrapper
+	@Published var detailActivity: SelectDetailResDto?
+	@Published var hasImage = false
+	@Published var mainImage: UIImage?
+	@Published var isShowToastMessage = false
+	@Published var isLoading = true
+	// MARK: - Porperties
+	private var cancellable: Set<AnyCancellable> = []
 
-    
+	
 	func fetchDetailActivity(id: String) {
-        guard let token = Constants.getKeychainValue(forKey: Constants.KeychainKey.token) else { return }
-        
-        APIClient.dispatch(
-            APIRouter.SelectDetailReqDto(
-                headers: APIHeader.Default(token: token),
-                body: APIParameters.SelectDetailReqDto(economicActivityNo: id)))
-        .sink { error in
-            switch error {
-            case .failure(let data):
-                switch data {
-                case .error4xx(_):
-                    print("400")
-                    break
-                case .error5xx(_):
-                    print("500")
-                    break
-                case .decodingError(_):
-                    print("디코딩에러")
-                    break
-                case .urlSessionFailed(_):
-                    print("url 세션 에러")
-                    break
-                case .timeOut:
-                    print("시간 초과")
-                    break
-                case .unknownError:
-                    print("unknownError")
-                    break
-                default:
-                    break
-                }
-            case .finished:
-                break
-            }
-        } receiveValue: { [weak self] response in
-            guard let self = self else { return }
-            self.detailActivity = response
-            self.isLoading = false
-        }.store(in: &cancellable)
-    }
+		guard let token = Constants.getKeychainValue(forKey: Constants.KeychainKey.token) else { return }
+		self.isLoading = true
+		APIClient.dispatch(
+			APIRouter.SelectDetailReqDto(
+				headers: APIHeader.Default(token: token),
+				body: APIParameters.SelectDetailReqDto(economicActivityNo: id)))
+		.sink { error in
+			switch error {
+			case .failure(let data):
+				switch data {
+				case .error4xx(_):
+					print("400")
+					break
+				case .error5xx(_):
+					print("500")
+					break
+				case .decodingError(_):
+					print("디코딩에러")
+					break
+				case .urlSessionFailed(_):
+					print("url 세션 에러")
+					break
+				case .timeOut:
+					print("시간 초과")
+					break
+				case .unknownError:
+					print("unknownError")
+					break
+				default:
+					break
+				}
+			case .finished:
+				break
+			}
+		} receiveValue: { [weak self] response in
+			guard let self = self else { return }
+			self.detailActivity = response
+			self.isLoading = false
+		}.store(in: &cancellable)
+	}
 	
 	func getMonthlyList(_ dateYM: String) {
 		guard let date = Int(dateYM), let token = Constants.getKeychainValue(forKey: Constants.KeychainKey.token) else { return }
@@ -78,7 +78,6 @@ final class HomeDetailViewModel {
 				}
 			}, receiveValue: { [weak self] response in
 				guard let self = self else { return }
-				print(dateYM)
 				// 이번 달만 위젯에 보여줌
 				if dateYM == Date().getFormattedYM() {
 					UserDefaults.shared.set(response.earn, forKey: "earn")

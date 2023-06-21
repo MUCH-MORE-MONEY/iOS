082bebdb5c91915636dc0884c4e76a0626d02a42
GeonhyeongKim <geonhyeong.dev@gmail.com>
2023-06-21 19:57:47 +0900

    [Feat] Firebase 등록

--- MMM/App/AppDelegate.swift
+++ MMM/App/AppDelegate.swift
@@ -6,11 +6,14 @@
 //
 
 import UIKit
+import FirebaseCore
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
 	func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+		FirebaseApp.configure()
+		
 		return true
 	}
 

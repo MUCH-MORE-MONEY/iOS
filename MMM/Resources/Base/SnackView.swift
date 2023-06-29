a26c017f9844f424e831841c3225efae3c9bc360
jwoo820 <pjwoo820@gmail.com>
2023-06-29 22:09:43 +0900

    [Feat] 스낵뷰 수정

--- MMM/Resources/Base/SnackView.swift
+++ MMM/Resources/Base/SnackView.swift
@@ -11,12 +11,12 @@ import Then
 import SnapKit
 
 final class SnackView: UIView {
-	// MARK: - UI Components
-	private lazy var textLabel = UILabel()
-	private lazy var retryButton = UIButton()
-	private lazy var stackView = UIStackView()
-	// MARK: - Properties
-	private var cancellable = Set<AnyCancellable>()
+    // MARK: - UI Components
+    private lazy var textLabel = UILabel()
+    private lazy var retryButton = UIButton()
+    private lazy var stackView = UIStackView()
+    // MARK: - Properties
+    private var cancellable = Set<AnyCancellable>()
     
     private var viewModel: AnyObject
     private var idList: [String]?
@@ -25,9 +25,9 @@ final class SnackView: UIView {
     init(viewModel: AnyObject) {
         self.viewModel = viewModel
         super.init(frame: .zero)
-		setup()
-	}
-	
+        setup()
+    }
+    
     
     convenience init(viewModel: AnyObject, idList: [String], index: Int) {
         self.init(viewModel: viewModel)
@@ -35,21 +35,21 @@ final class SnackView: UIView {
         self.index = index
     }
     
-	required init?(coder: NSCoder) {
-		fatalError("init(coder:) has not been implemented")
-	}
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 }
 
 extension SnackView {
-	private func setup() {
-		bind()
-		setAttribute()
-		setLayout()
-	}
-	
-	private func bind() {
-		retryButton.tapPublisher
-			.sinkOnMainThread { [weak self] in
+    private func setup() {
+        bind()
+        setAttribute()
+        setLayout()
+    }
+    
+    private func bind() {
+        retryButton.tapPublisher
+            .sinkOnMainThread { [weak self] in
                 guard let self = self else { return }
                 
                 switch viewModel {
@@ -61,31 +61,31 @@ extension SnackView {
                 default:
                     print("error type")
                 }
-
-			}.store(in: &cancellable)
-	}
-	
-	private func setAttribute() {
-		addSubviews(stackView)
-		stackView.addArrangedSubviews(textLabel, retryButton)
-		
-		stackView = stackView.then {
-			$0.axis = .horizontal
-			$0.spacing = 16
-			$0.alignment = .fill
+                
+            }.store(in: &cancellable)
+    }
+    
+    private func setAttribute() {
+        addSubviews(stackView)
+        stackView.addArrangedSubviews(textLabel, retryButton)
+        
+        stackView = stackView.then {
+            $0.axis = .horizontal
+            $0.spacing = 16
+            $0.alignment = .fill
             $0.distribution = .equalSpacing
-		}
-		
-		textLabel = textLabel.then {
-			$0.text = "일시적인 오류가 발생했습니다."
-			$0.numberOfLines = 2
-			$0.textColor = R.Color.white
-			$0.font = R.Font.body1
-		}
-		
-		retryButton = retryButton.then {
-			$0.setTitle("재시도", for: .normal)
-			$0.setTitleColor(R.Color.orange500, for: .normal)
+        }
+        
+        textLabel = textLabel.then {
+            $0.text = "일시적인 오류가 발생했습니다."
+            $0.numberOfLines = 2
+            $0.textColor = R.Color.white
+            $0.font = R.Font.body1
+        }
+        
+        retryButton = retryButton.then {
+            $0.setTitle("재시도", for: .normal)
+            $0.setTitleColor(R.Color.orange500, for: .normal)
             $0.titleLabel?.font = R.Font.title3
             $0.setTitleColor(R.Color.gray500, for: .highlighted)
 		}
@@ -104,5 +104,5 @@ extension SnackView {
          retryButton.snp.makeConstraints {
             $0.right.equalToSuperview().offset(16)
         }
-	}
+    }
 }

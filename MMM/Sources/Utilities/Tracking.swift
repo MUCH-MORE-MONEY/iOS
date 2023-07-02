1ab49711e4bcb0fe6d0a1d2863cd15aab289cdd1
jwoo820 <pjwoo820@gmail.com>
2023-07-02 13:51:11 +0900

    [Feat] ga 트레킹을 위한 열거형 타입 선언

---
+++ MMM/Sources/Utilities/Tracking.swift
@@ -0,0 +1,22 @@
+//
+//  Tracking.swift
+//  MMM
+//
+//  Created by Park Jungwoo on 2023/07/02.
+//
+
+import Foundation
+
+enum Tracking {
+    enum Screen {
+        static let home = "홈"
+        static let mypage = "마이 페이지"
+        static let login = "로그인"
+    }
+    
+    enum Event {
+        static let touchAddView = "추가하기 뷰 클릭"
+        static let touchDetailView = "상세뷰 클릭"
+        static let touchEditView = "편집하기 클릭"
+    }
+}

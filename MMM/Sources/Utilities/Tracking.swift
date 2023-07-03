2a56cc5760d3f5d7c000919371a51589feadef49
jwoo820 <pjwoo820@gmail.com>
2023-07-03 11:24:25 +0900

    [Feat] ga tracking 타입 정의

--- MMM/Sources/Utilities/Tracking.swift
+++ MMM/Sources/Utilities/Tracking.swift
@@ -21,4 +21,23 @@ enum Tracking {
         static let deleteActivity = "경제활동 내역 삭제"
         static let insertActivity = "경제활동 내역 추가"
     }
+    
+    enum FinActAddPage {
+        static let start = "FinActAddPage_start"
+        static let viewDetail = "FinActAddPage_view_detail"
+        static let complete = "FinActAddPage_complete"
+        
+        static let nextBtnRating = "FinActAddPage_nextbtn_rating"
+        static let nextBtnCategory = "FinActAddPage_nextbtn_category"
+        static let nextBtnDate = "FinActAddPage_nextbtn_date"
+        static let nextBtnAmount = "FinActAddPage_nextbtn_amount"
+        
+        static let inputCategory = "FinActAddPage_input_category"
+        static let inputDate = "FinActAddPage_input_date"
+        static let inputAmount = "FinActAddPage_input_amount"
+        static let inputTitle = "FinActAddPage_input_title"
+        static let inputPhoto = "FinActAddPage_input_photo"
+        static let inputRating = "FinActAddPage_input_rating"
+        static let inputMemo = "FinActAddPage_input_memo"
+    }
 }

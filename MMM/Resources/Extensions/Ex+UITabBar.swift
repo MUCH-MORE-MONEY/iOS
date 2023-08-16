ce4542460cb7499ecea0fa01d33224d0aca448af
jwoo820 <pjwoo820@gmail.com>
2023-08-16 21:11:38 +0900

    [Feat] 탭바 UI 변경

--- MMM/Resources/Extensions/Ex+UITabBar.swift
+++ MMM/Resources/Extensions/Ex+UITabBar.swift
@@ -26,16 +26,15 @@ extension CALayer {
 
 extension UITabBar {
     // 기본 그림자 스타일을 초기화해야 커스텀 스타일을 적용할 수 있다.
-    static func clearShadow() {
+    func setTabShadow() {
+        self.layer.masksToBounds = false
+        self.clearShadow()
+        self.layer.applyShadow()
+    }
+    
+    func clearShadow() {
         UITabBar.appearance().shadowImage = UIImage()
         UITabBar.appearance().backgroundImage = UIImage()
         UITabBar.appearance().backgroundColor = UIColor.white
-    }
-}
-
-extension UIView {
-    func setTabShadow() {
-        self.layer.masksToBounds = false
-        self.layer.applyShadow()
     }
 }

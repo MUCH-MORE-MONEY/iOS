1dcdb4c1b39b0f043c3d75ce00840439cf068140
GeonhyeongKim <geonhyeong.dev@gmail.com>
2023-08-21 17:30:56 +0900

    [Feat] 통계 탭의 navigation leftBarButtonItem 설정

--- MMM/Sources/ViewController/Statistics/StatisticsViewController.swift
+++ MMM/Sources/ViewController/Statistics/StatisticsViewController.swift
@@ -34,9 +34,7 @@ final class StatisticsViewController: UIViewController, View {
         super.viewDidLoad()
 		setup()		// 초기 셋업할 코드들
     }
-}
-//MARK: - Style & Layouts
-extension StatisticsViewController {
+	
 	// 초기 셋업할 코드들
 	private func setup() {
 		setAttribute()
@@ -47,10 +45,11 @@ extension StatisticsViewController {
 		bindState(reactor)
 		bindAction(reactor)
 	}
-	
+}
+//MARK: - Bind
+extension StatisticsViewController {
 	// MARK: 데이터 변경 요청 및 버튼 클릭시 요청 로직(View -> Reactor)
 	private func bindAction(_ reacotr: StatisticsReactor) {
-	
 	}
 	
 	// MARK: 데이터 바인딩 처리 (Reactor -> View)
@@ -70,8 +69,34 @@ extension StatisticsViewController {
 //			}
 //			.disposed(by: disposeBag)
 	}
-		
+}
+//MARK: - Style & Layouts
+extension StatisticsViewController {
 	private func setAttribute() {
+		view.backgroundColor = R.Color.gray900
+		
+		// Root View인 NavigationView에 item 수정하기
+		if let navigationController = self.navigationController {
+			if let rootVC = navigationController.viewControllers.first {
+				rootVC.navigationItem.leftBarButtonItem = monthButtonItem
+			}
+		}
+		
+		monthButton = monthButton.then {
+			$0.frame = .init(origin: .zero, size: .init(width: 150, height: 24))
+			$0.setTitle(Date().getFormattedDate(format: "M월"), for: .normal)
+			$0.setImage(R.Icon.arrowExpandMore16, for: .normal)
+			$0.setTitleColor(R.Color.white, for: .normal)
+			$0.setTitleColor(R.Color.white.withAlphaComponent(0.7), for: .highlighted)
+			$0.imageView?.contentMode = .scaleAspectFit
+			$0.titleLabel?.font = R.Font.h5
+			$0.contentHorizontalAlignment = .left
+			$0.imageEdgeInsets = .init(top: 0, left: 11, bottom: 0, right: 0) // 이미지 여백
+		}
+		
+		monthButtonItem = monthButtonItem.then {
+			$0.customView = monthButton
+		}
 	}
 	
 	private func setLayout() {

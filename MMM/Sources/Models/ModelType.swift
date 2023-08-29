05245ebaec86dc4f002b84a918dba0cb64e4263d
jwoo820 <pjwoo820@gmail.com>
2023-08-29 15:43:46 +0900

    [Feat] Request를 위한 프로토콜 정의

---
+++ MMM/Sources/Models/ModelType.swift
@@ -0,0 +1,10 @@
+//
+//  ModelType.swift
+//  MMM
+//
+//  Created by Park Jungwoo on 2023/08/29.
+//
+
+import Then
+
+protocol ModelType: Then, Codable, Equatable {}
---
+++ MMM/Sources/Models/Requests/PushRequest.swift
@@ -0,0 +1,10 @@
+//
+//  PushRequest.swift
+//  MMM
+//
+//  Created by Park Jungwoo on 2023/08/29.
+//
+
+import Foundation
+
+struct PushRequest: ModelType { }

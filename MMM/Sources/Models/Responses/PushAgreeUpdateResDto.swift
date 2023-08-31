6ebc83f4e0ff865f7eca7b2a16d9edf39eff1714
jwoo820 <pjwoo820@gmail.com>
2023-08-31 13:56:27 +0900

    [Feat]  PushUpdate res 모델 정의

---
+++ MMM/Sources/Models/Responses/PushAgreeUpdateResDto.swift
@@ -0,0 +1,12 @@
+//
+//  PushAgreeUpdateResDto.swift
+//  MMM
+//
+//  Created by Park Jungwoo on 2023/08/31.
+//
+
+import Foundation
+
+struct PushAgreeUpdateResDto: Decodable {
+    var message: String
+}

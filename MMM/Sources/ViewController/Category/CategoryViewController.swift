4f1707f1b10dc645996f5a98a69e3a12fad40012
GeonhyeongKim <geonhyeong.dev@gmail.com>
2023-09-07 16:39:24 +0900

    [Feat] PageViewController와 함께 UISegmentedControl 결합

--- MMM/Sources/ViewController/Category/CategoryViewController.swift
+++ MMM/Sources/ViewController/Category/CategoryViewController.swift
@@ -16,9 +16,23 @@ final class CategoryViewController: BaseViewController, View {
 	
 	// MARK: - Properties
 	var disposeBag: DisposeBag = DisposeBag()
-
+	private lazy var currentPage: Int = 0 {
+		didSet {
+			// from segmentedControl -> pageViewController 업데이트
+			let direction: UIPageViewController.NavigationDirection = oldValue <= self.currentPage ? .forward : .reverse
+			self.pageViewController.setViewControllers([dataViewControllers[self.currentPage]], direction: direction, animated: true, completion: nil)
+		}
+	}
+	
 	// MARK: - UI Components
-
+	private lazy var segmentedControl = CategorySegmentedControl(items: ["지출", "수입"])
+	private lazy var pageViewController = UIPageViewController()
+	private lazy var payViewController = UIViewController()
+	private lazy var earnViewController = UIViewController()
+	private var dataViewControllers: [UIViewController] {
+		[self.payViewController, self.earnViewController]
+	}
+	
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		setup()		// 초기 셋업할 코드들
@@ -31,6 +45,10 @@ final class CategoryViewController: BaseViewController, View {
 }
 //MARK: - Action
 extension CategoryViewController {
+	@objc private func changeValue(control: UISegmentedControl) {
+	  // 코드로 값을 변경하면 해당 메소드 호출 x
+	  self.currentPage = control.selectedSegmentIndex
+	}
 }
 //MARK: - Bind
 extension CategoryViewController {
@@ -53,8 +71,61 @@ extension CategoryViewController {
 	private func setAttribute() {
 		view.backgroundColor = R.Color.gray900
 		title = "카테고리"
+		
+		segmentedControl = segmentedControl.then {
+			$0.selectedSegmentIndex = 0
+			$0.translatesAutoresizingMaskIntoConstraints = true
+			$0.setTitleTextAttributes([.foregroundColor: R.Color.gray700, .font: R.Font.title1], for: .normal)
+			$0.setTitleTextAttributes([.foregroundColor: R.Color.white, .font: R.Font.title1], for: .selected)
+			$0.addTarget(self, action: #selector(changeValue(control:)), for: .valueChanged)
+		}
+		
+		pageViewController = UIPageViewController(transitionStyle: .scroll, navigationOrientation: .horizontal, options: nil).then {
+			$0.setViewControllers([self.dataViewControllers[0]], direction: .forward, animated: true)
+			$0.delegate = self
+			$0.dataSource = self
+			$0.view.translatesAutoresizingMaskIntoConstraints = false
+		}
+		
+		payViewController = payViewController.then {
+			$0.view.backgroundColor = R.Color.gray500
+		}
+		
+		earnViewController = earnViewController.then {
+			$0.view.backgroundColor = R.Color.gray600
+		}
 	}
 	
 	private func setLayout() {
+		view.addSubviews(segmentedControl, pageViewController.view)
+		
+		segmentedControl.snp.makeConstraints {
+			$0.top.equalToSuperview()
+			$0.leading.trailing.equalToSuperview().inset(24)
+			$0.height.equalTo(50)
+		}
+		
+		pageViewController.view.snp.makeConstraints {
+			$0.top.equalTo(segmentedControl.snp.bottom)
+			$0.leading.trailing.bottom.equalToSuperview()
+		}
+	}
+}
+//MARK: - UIPageViewController DataSource, Delegate
+extension CategoryViewController: UIPageViewControllerDataSource, UIPageViewControllerDelegate {
+	func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
+		guard let index = self.dataViewControllers.firstIndex(of: viewController), index - 1 >= 0 else { return nil }
+		return self.dataViewControllers[index - 1]
+	}
+	
+	func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController? {
+		guard let index = self.dataViewControllers.firstIndex(of: viewController), index + 1 < self.dataViewControllers.count else { return nil }
+		return self.dataViewControllers[index + 1]
+	}
+	
+	func pageViewController(_ pageViewController: UIPageViewController, didFinishAnimating finished: Bool,previousViewControllers: [UIViewController], transitionCompleted completed: Bool) {
+		guard let viewController = pageViewController.viewControllers?[0], let index = self.dataViewControllers.firstIndex(of: viewController) else { return }
+		self.currentPage = index
+		self.segmentedControl.selectedSegmentIndex = index
 	}
 }

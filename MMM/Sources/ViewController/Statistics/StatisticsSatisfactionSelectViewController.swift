9ac5bfabbe415462d38938313c89af942e18ad4b
GeonhyeongKim <geonhyeong.dev@gmail.com>
2023-09-19 04:47:27 +0900

    [Rename] 코드 이동

--- MMM/Sources/ViewController/Statistics/StatisticsSatisfactionSelectViewController.swift
+++ MMM/Sources/ViewController/Statistics/StatisticsSatisfactionSelectViewController.swift
@@ -11,6 +11,29 @@ import SnapKit
 import ReactorKit
 import RxRelay
 
+enum Satisfaction: Int {
+	case low	// 1~2점
+	case middle	// 3점
+	case hight	// 4~5점
+	
+	var title: String {
+		switch self {
+		case .low: return "아쉬운 활동 줄이기"
+		case .middle: return "평범한 활동 돌아보기"
+		case .hight: return "만족스러운 활동 늘리기"
+		}
+	}
+	
+	var score: String {
+		switch self {
+		case .low: return "1~2점"
+		case .middle: return "3점"
+		case .hight: return "4~5점"
+		}
+	}
+}
+
+// 상속하지 않으려면 final 꼭 붙이기
 final class StatisticsSatisfactionSelectViewController: UIViewController, View {
 	typealias Reactor = BottomSheetReactor
 

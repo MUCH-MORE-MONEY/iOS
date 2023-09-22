3f955bf4de48f2bac412b63d32776764596a0804
GeonhyeongKim <geonhyeong.dev@gmail.com>
2023-09-23 03:15:16 +0900

    [Feat] 가격 콤마와 "원" 표시

--- MMM/Sources/ViewController/Category/Views/CategoryCollectionViewCell.swift
+++ MMM/Sources/ViewController/Category/Views/CategoryCollectionViewCell.swift
@@ -41,7 +41,7 @@ extension CategoryCollectionViewCell {
 	// MARK: 데이터 변경 요청 및 버튼 클릭시 요청 로직(View -> Reactor)
 	private func bindAction(_ reactor: CategoryCollectionViewCellReactor) {
 		titleLabel.text = reactor.currentState.category.title
-		priceLabel.text = reactor.currentState.category.price
+		priceLabel.text = (Int(reactor.currentState.category.price) ?? 0).withCommas() + " 원"
 	}
 	
 	// MARK: 데이터 바인딩 처리 (Reactor -> View)

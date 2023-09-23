c2615cc5f7bef35ffd95f00712837a27c2e05f5e
GeonhyeongKim <geonhyeong.dev@gmail.com>
2023-09-23 21:31:24 +0900

    [Feat] 카테고리 TableView item bind

--- MMM/Sources/ViewController/Category/CategoryDetailViewController.swift
+++ MMM/Sources/ViewController/Category/CategoryDetailViewController.swift
@@ -21,7 +21,8 @@ final class CategoryDetailViewController: BaseViewControllerWithNav, View {
 	// MARK: - Properties
 	
 	// MARK: - UI Components
-	
+	private lazy var tableView = UITableView()
+
 	override func viewDidLoad() {
 		super.viewDidLoad()
 	}
@@ -35,6 +36,14 @@ final class CategoryDetailViewController: BaseViewControllerWithNav, View {
 extension CategoryDetailViewController {
 	// MARK: 데이터 변경 요청 및 버튼 클릭시 요청 로직(View -> Reactor)
 	private func bindAction(_ reactor: CategoryDetailReactor) {
+		// TableView cell select
+//		Observable.zip(
+//			tableView.rx.itemSelected,
+//			tableView.rx.modelSelected(EconomicActivity.self)
+//		)
+//		.map { .selectCell($0, $1) }
+//		.bind(to: reactor.action)
+//		.disposed(by: disposeBag)
 	}
 	// MARK: 데이터 바인딩 처리 (Reactor -> View)
 	private func bindState(_ reactor: CategoryDetailReactor) {
@@ -46,6 +55,24 @@ extension CategoryDetailViewController {
 				self.title = category.title
 			})
 			.disposed(by: disposeBag)
+		
+		reactor.state
+			.map { $0.list }
+			.distinctUntilChanged() // 중복값 무시
+			.bind(to: tableView.rx.items) { tv, row, data in
+				let index = IndexPath(row: row, section: 0)
+				let cell = tv.dequeueReusableCell(withIdentifier: HomeTableViewCell.className, for: index) as! HomeTableViewCell
+				
+				// 데이터 설정
+				cell.setData(data: data, last: row == reactor.currentState.list.count - 1)
+				cell.backgroundColor = R.Color.gray100
+
+				let backgroundView = UIView()
+				backgroundView.backgroundColor = R.Color.gray400.withAlphaComponent(0.3)
+				cell.selectedBackgroundView = backgroundView
+				
+				return cell
+			}.disposed(by: disposeBag)
 	}
 }
 //MARK: - Action
@@ -56,13 +83,27 @@ extension CategoryDetailViewController {
 	// 초기 셋업할 코드들
 	override func setAttribute() {
 		super.setAttribute()
+		
+		tableView = tableView.then {
+			$0.register(HomeTableViewCell.self)
+			$0.backgroundColor = R.Color.gray100
+			$0.showsVerticalScrollIndicator = false
+			$0.separatorStyle = .none
+			$0.rowHeight = UITableView.automaticDimension
+		}
 	}
 	
 	override func setHierarchy() {
 		super.setHierarchy()
+		
+		view.addSubviews(tableView)
 	}
 	
 	override func setLayout() {
 		super.setLayout()
+		
+		tableView.snp.makeConstraints {
+			$0.edges.equalToSuperview()
+		}
 	}
 }

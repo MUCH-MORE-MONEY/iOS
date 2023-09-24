85c42254d3512f673fd7723946e9a4d2399af6f3
GeonhyeongKim <geonhyeong.dev@gmail.com>
2023-09-24 22:59:11 +0900

    [Feat] 카테고리 모델 정의

---
+++ MMM/Sources/Models/Category.swift
@@ -0,0 +1,39 @@
+//
+//  Category.swift
+//  MMM
+//
+//  Created by geonhyeong on 2023/09/24.
+//
+
+import Foundation
+
+struct Category: Codable, Equatable {
+	let id, title, dateYM: String
+	let total, orderNum, ratio: Int
+	let lowwer: [CategoryLowwer]
+
+	// 파라미터 이름 변경
+	enum CodingKeys: String, CodingKey {
+		case id = "upperEconomicActivityCategoryCd"
+		case title = "upperEconomicActivityCategoryNm"
+		case total = "upperEconomicActivityCategorySumAmt"
+		case orderNum = "upperOrderNum"
+		case dateYM = "dateYM"
+		case ratio = "economicActivityCategoryRatio"
+		case lowwer = "selectListMonthlyEconomicActivityByCategoryOutputDto"
+	}
+}
+
+struct CategoryLowwer: Codable, Equatable {
+	let id, title: String
+	let total, orderNum: Int
+	
+	// 파라미터 이름 변경
+	enum CodingKeys: String, CodingKey {
+		case id = "economicActivityCategoryCd"
+		case title = "economicActivityCategoryNm"
+		case total = "economicActivityCategorySumAmt"
+		case orderNum = "orderNum"
+	}
+}
+

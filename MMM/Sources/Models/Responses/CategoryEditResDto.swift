9b46f1b48e51914e4848559372da941284c93806
GeonhyeongKim <geonhyeong.dev@gmail.com>
2023-09-24 22:59:56 +0900

    [Feat] 카테고리 조회 Response 정의

--- MMM/Sources/Models/Responses/CategoryEditResDto.swift
+++ MMM/Sources/Models/Responses/CategoryEditResDto.swift
@@ -21,6 +21,13 @@ struct CategoryEditHeaderResDto: Codable {
 	var status: String
 }
 
+/// 경제활동구분 코드 기준 카테고리별 월간 경제활동 목록 전체 조회 Response를 위한 데이터 타입
+struct CategoryListResDto: Decodable {
+	var data: SelectListMonthlyByCategoryCdOutputDto
+	var message: String?
+	var status: String
+}
+
 /// 카테고리코드 기준 월간 경제활동 상세 목록 조회 Response를 위한 데이터 타입
 struct CategoryDetailListResDto: Decodable {
 	var data: SelectListMonthlyByCategoryCdOutputDto

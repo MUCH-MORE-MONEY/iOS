3736afd618cbf04f2d9043b039c4195f5bb586c2
GeonhyeongKim <geonhyeong.dev@gmail.com>
2023-10-29 22:14:41 +0900

    [Refactor] 카테고리 Header 위치 조절

--- MMM/Sources/ViewController/CategoryEdit/CategoryEditViewController.swift
+++ MMM/Sources/ViewController/CategoryEdit/CategoryEditViewController.swift
@@ -260,12 +260,12 @@ extension CategoryEditViewController {
 		group.contentInsets = .init(top: 0, leading: 136, bottom: 0, trailing: 0)
 		
 		// 홀/짝을 계산해서 top inset 계산
-		let insetBase: CGFloat = CGFloat(count / 2) * (UI.cellHeightMargin)
-		let insetByOdd: CGFloat = insetBase + UI.cellSpacingMargin / 2 + 1
-		let insetByEven: CGFloat = insetBase - UI.cellSpacingMargin / 2 - 3
+//		let insetBase: CGFloat = CGFloat(count / 2) * (UI.cellHeightMargin)
+//		let insetByOdd: CGFloat = insetBase + UI.cellSpacingMargin / 2 + 1
+//		let insetByEven: CGFloat = insetBase - UI.cellSpacingMargin / 2 - 3
 		
-		let header = NSCollectionLayoutBoundarySupplementaryItem(layoutSize: .init(widthDimension: .fractionalWidth(0.395), heightDimension: .absolute(UI.cellHeightMargin)), elementKind: UICollectionView.elementKindSectionHeader, alignment: .leading)
-		header.contentInsets = .init(top: isEven ? -insetByEven : -insetByOdd, leading: 0, bottom: 0, trailing: 0)
+		let header = NSCollectionLayoutBoundarySupplementaryItem(layoutSize: .init(widthDimension: .fractionalWidth(0.395), heightDimension: .absolute(UI.cellHeightMargin)), elementKind: UICollectionView.elementKindSectionHeader, alignment: .topLeading)
+		header.contentInsets = .init(top: 34, leading: 0, bottom: 0, trailing: 0)
 		
 		let footer = NSCollectionLayoutBoundarySupplementaryItem(layoutSize: .init(widthDimension: .fractionalWidth(1), heightDimension: .absolute(UI.footerHeight)), elementKind: UICollectionView.elementKindSectionFooter, alignment: .bottom)
 		

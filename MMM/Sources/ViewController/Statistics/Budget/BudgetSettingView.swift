eafe416dd9f1beec3cc5469cacd09a607bf1936a
Jungwoo Park <48409306+jwoo820@users.noreply.github.com>
2024-02-20 15:16:21 +0900

    [Feat] 예산 설정 segment 추가

--- MMM/Sources/ViewController/Statistics/Budget/BudgetSettingView.swift
+++ MMM/Sources/ViewController/Statistics/Budget/BudgetSettingView.swift
@@ -9,7 +9,13 @@ import SwiftUI
 
 struct BudgetSettingView: View {
     var body: some View {
-        Text("예산 설정")
+        VStack {
+            SegmentedView()
+                .padding(.top, 16)
+                .padding([.leading, .trailing], 24)
+            Spacer()
+        }
+        .background(Color(R.Color.gray900))
     }
 }
 
@@ -17,9 +23,47 @@ struct BudgetSettingView: View {
     BudgetSettingView()
 }
 
+struct SegmentedView: View {
+
+    let segments: [String] = ["OPEN", "COMPLETED", "CANCELLED", "ALL", "Last"]
+    @State private var selected: String = "OPEN"
+    @Namespace var name
+
+    var body: some View {
+        HStack(spacing: 0) {
+            ForEach(segments, id: \.self) { segment in
+                Button {
+                    selected = segment
+                } label: {
+                    VStack {
+                        ZStack {
+                            Capsule()
+                                .fill(Color(R.Color.gray600))
+                                .frame(height: 2)
+                            if selected == segment {
+                                Capsule()
+                                    .fill(selected == segment ? Color(R.Color.white) : Color(R.Color.gray600))
+                                    .frame(height: 2)
+                                    .matchedGeometryEffect(id: "Tab", in: name)
+                            }
+                        }
+                        .padding([.leading, .trailing], 1)
+                    }
+                }
+            }
+        }
+    }
+}
+
+final class BudgetSettingHostingController: UIHostingController<BudgetSettingView> {
+    override func viewDidLoad() {
+        
+    }
+}
+
 final class BudgetSettingViewInterface {
     func budgetSettingViewUI() -> UIViewController {
-        let view = BudgetSettingView()
-        return UIHostingController(rootView: view)
+        let vc = BudgetSettingHostingController(rootView: BudgetSettingView())
+        return vc
     }
 }
